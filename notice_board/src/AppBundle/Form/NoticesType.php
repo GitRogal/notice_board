51d28d715cd9984622c864b6f6f4557eef9a9d62
Tomasz Rogalski <rogalthomas@op.pl>
2019-08-23 13:02:42 +0200

    cosmetic changes

--- notice_board/src/AppBundle/Form/NoticesType.php
+++ notice_board/src/AppBundle/Form/NoticesType.php
@@ -2,10 +2,15 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Categories;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class NoticesType extends AbstractType
 {
@@ -15,9 +20,20 @@ class NoticesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('date')
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => 'name',
+                'label' => 'Wybierz kategorię'
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Tytuł'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Opis'
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Data'
+            ])
             ->add('imageName', FileType::class, [
                 'mapped' => false,
                 'label' => 'Dodaj zdjęcie'
